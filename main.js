81387afda32e40c16a2914c50bc5448dd4cfef84
HyeYoung-Shin <shinhy8101@naver.com>
2017-11-20 19:49:43 +0900

    before removing conditional chat

--- main.js
+++ main.js
@@ -1,30 +1,165 @@
 var express = require('express');
 var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io').listen(server);
-var bodyParser = require('body-parser');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var mongoose = require('mongoose');
+var passport = require('passport');
+var session = require('express-session');
+var flash = require('connect-flash');
+var async = require('async');
+/*
+mongoose.connect(process.env.MONGO_DB);
+var db = mongoose.connection;
+db.once("open", function(){
+  console.log("DB connected");
+});
+db.on("error", function (err){
+  console.log("DB ERROR :", err);
+});
+var dataSchema = mongoose.Schema({
+  name:String,
+  count:Number
+});
+var Data = mongoose.model('data', dataSchema);
+Data.findOne({name:"myData"}, function(err, data){
+  if(err) return console.log("Data ERROR:" , err);
+  if(!err){
+    Data.create({name:"myData", count:0}, function(err, data){
+      if(err) return console.log("Data ERROR:", err);
+      console.log("Counter initialized :", data);
+    });
+  }
+});
+
+//model setting
+var userSchema = mongoose.Schema({
+  email: {type:String, required:true, unique:true},
+  id: {type:String, required:true, unique:true},
+  password: {type:String, required:true},
+  age: {type:Number, required:true},
+//  gender: {type:Char, require:true},
+  createdAt: {type:Date, default:Date.now}
+});
+var User = mongoose.model('user', userSchema);
+*/
 
 var fs = require('fs');
-var path = require('path');
 
-var count =0;
-var socketRoom = {};
-
-app.set('port', 3000);
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.json());
-
-app.start = app.listen = app.aaa = function(){
-  return server.listen.apply(server, arguments);
-}
-
-app.aaa(app.get('port'), function(){
-  console.log("server start");
-});
+app.set("view engine", 'ejs');
 
 app.get("/", function(req, res){
   res.sendfile("public/main.html");
 });
+app.get("/randomchat", function(req, res){
+  res.sendfile("public/randomchat.html");
+});
+app.get("/useCondChat", function(req, res){
+  res.sendfile("public/useCondChat.html");
+});
+// set middleware
+app.use(flash());
+
+/*
+app.use(session({secret:'MySecret'}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function(user, done){
+  done(null, user.id);
+});
+passport.deserializeUser(function(id, done){
+  User.findById(id, function(err, user){
+    done(err, user);
+  });
+});
+
+var LocalStrategy = require('passport-local').Strategy;
+passport.use('local-login',
+  new LocalStrategy({
+    usernameField : 'id',
+    passwordField : 'password',
+    passReqToCallback : true
+    },
+    function(req, id, password, done){
+      User.findOne({'id' : id}, function(err, user){
+        if(err) return done(err);
+        if(!user){
+          req.flash("id", req.body.id);
+          return done(null, false, req.flash('loginError', 'No user found.'));
+        }
+        if(user.password != password){
+          req.flash("id", req.body.id);
+          return done(null, false, req.flash('loginError', 'Password does not match.'));
+        }
+        return done(null, user);
+      });
+    }
+  )
+);
+
+app.get('/login', function(req,res){
+  res.render('login/login', {id:req.flash("id")[0], loginError:req.flash('loginError')});
+});
+app.post('/login',
+  function(req,res,next){
+    req.flash("id");
+    if(req.body.id.length === 0 || req.body.password.length === 0){
+      req.flash("id", req.body.id);
+      req.flash("loginError", "Please enter both id and password.");
+      res.redirect('/login');
+    } else{
+      next();
+    }
+  }, passport.authenticate('local-login', {
+    successRedirect : '/condChat',
+    failureRedirect : '/login',
+    failureFlash : true
+  })
+);
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
+app.get('/users/new', function(req,res){
+  res.render('users/new', {
+    formData: req.flash('formData')[0],
+    emailError: req.flash('emailError')[0],
+    idError: req.flash('idError')[0],
+    passwordError: req.flash('passwordError')[0]
+    }
+  );
+});
+app.post('/users', checkUserRegValidation, function(req, res, next){
+  User.create(req.body.user, function(err, user) {
+    if(err) return res.json({success:false, message:err});
+    res.redirect('/login');
+  });
+});
+app.get('/users/:id', function(req,res){
+  User.findById(req.params.id, function(err, user){
+    if(err) return res.json({success:false, message:err});
+    res.render("users/show", {user: user});
+  });
+});
+//user edit
+app.get('/users/:id/edit', function(req,res){
+  User.findById(req.params.id, function(err, user){
+    if(err) return res.json({success:false, message:err});
+    res.render("users/edit", {
+      user: user,
+      formData: req.flash('formData')[0],
+      emailError: req.flash('emailError')[0],
+      idError: req.flash('idError')[0],
+      passwordError: req.flash('passwordError')[0]
+      }
+    );
+  });
+});
+*/
+
+
+var roomnum =1;
 
 function include(file_){
   with (global){
@@ -42,30 +177,7 @@ app.all('*', function(req, res, next){
   next();
 })
 
-/*
-app.set("view engine", 'ejs');
-app.use(express.static(path.join(__dirname, 'public'))); //static폴더 세팅
 
- var data={count:0};
-app.get('/', function (req,res) {
-//    console.log(req);
-    console.log(res);
-    data.count++;
-    res.render('Hye Young chat application', data);
+http.listen('3000', function(){
+  console.log("server on");
 });
-
-app.get('/reset', function (req,res) {
-    data.count=0;
-    res.render('firstejs', data);
-});
-app.get('/set/count', function (req,res) {
-    if(req.query.count) data.count = req.query.count;
-    res.render('firstejs', data);
-});
-
-*/
-/*
-server.listen(3000, function(){
-  console.log('Server On!');
-});
-*/
--- service/ChatServer.js
+++ service/ChatServer.js
@@ -1,47 +1,45 @@
-
 io.sockets.on('connection', function(socket){
-  var count =1;
-
   console.log('user connected: ', socket.id);
-  var name = "user" + count++;
+
+  var usernum=0;
+  var roomnum =0;
+
+  while(io.sockets.adapter.rooms["room-"+roomnum] && io.sockets.adapter.rooms["room-"+roomnum].length > 1 ){
+    roomnum++;
+  }
+
+  socket.join("room-"+roomnum);
+  console.log('connected room No.', roomnum);
+  console.log('room length: ', io.sockets.adapter.rooms["room-"+roomnum].length);
+
+  if(io.sockets.adapter.rooms["room-"+roomnum].length == 1)
+      usernum = 1;
+  if(io.sockets.adapter.rooms["room-"+roomnum].length == 2)
+      usernum = 2;
+
+  var name = "user" + usernum;
   io.to(socket.id).emit('change name', name);
+  io.sockets.in("room-"+roomnum).emit('receive message', name+"님이 입장했습니다.");
 
-  socket.on('random_request', function(){
-    console.log('request to make Random chat');
-
-    var rooms = io.sockets.manager.rooms;
-         for (var key in rooms){
-             if (key == ''){
-                 continue;
-             }
-
-             if (rooms[key].length == 1){
-                 var roomKey = key.replace('/', '');
-                 socket.join(roomKey);
-                 io.sockets.in(roomKey).emit('completeMatch', {});
-                 socketRoom[socket.id] = roomKey;
-                 return;
-             }
-         }
-
-         socket.join(socket.id);
-         socketRoom[socket.id] = socket.id;
-/*
-    socket.join(data.roomname);
-    socket.set('room', data.roomname);
-    socket.set('username', data.username);
-    */
-  });
 
   socket.on('disconnect', function(){
+    socket.leave("room-"+roomnum);
     console.log('user disconnected: ', socket.id);
-    io.emit('disconnected', name);
+    socket.broadcast.to("room-"+roomnum).emit('disconnected', name);
+  });
+
+  socket.on('room disconnect', function(){
+    socket.leave("room-"+roomnum);
+    console.log('room disconnected ');
   });
 
   socket.on('send message', function(name,text){
     var msg = name + ' : '+ text;
-    console.log(msg);
-    io.emit('receive message', msg);
+    var trouble = 0;
+//    console.log(msg);
+    if(text == "ㅂㅅ" || "ㅄ"){
+      trouble = 1;
+    }
+    io.sockets.in("room-"+roomnum).emit('receive message', msg, trouble);
   });
-
 });
